ccd66046ad396099afcdeb837d58cb9c697070d5
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-01-16 16:19:23 -0500

    Completed Project #8

--- 8-menu/setup/app.js
+++ 8-menu/setup/app.js
@@ -71,4 +71,84 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+const buttonContainer = document.querySelector(".btn-container");
+
+// on page load, load all menu items and
+// run filterButtons fn which will display all unique categories as filter buttons
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  filterButtons();
+});
+
+// display menu items based on the array of items passed in
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return `<article class="menu-item">
+    <img src=${item.img} class="photo" alt=${item.title} />
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">
+        ${item.desc}
+      </p>
+    </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+}
+
+function filterButtons() {
+  // dynamically create array of all unique categories
+  const categories = menu.map(function (item) {
+    return item.category;
+  });
+  uniqueCategories = ["all", ...new Set(categories)];
+
+  // reduce method may also be used to obtain unique category values
+  // const categories = menu.reduce(
+  //   function (values, item) {
+  //     if (!values.includes(item.category)) {
+  //       values.push(item.category);
+  //     }
+  //     return values;
+  //   },
+  //   ["all"]
+  // );
+
+  // display only unique category filter buttons
+  let displayButtons = uniqueCategories.map(function (category) {
+    return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`;
+  });
+  displayButtons = displayButtons.join("");
+  buttonContainer.innerHTML = displayButtons;
+  const filterBtns = document.querySelectorAll(".filter-btn");
+
+  //filter menu items to display
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      category === "all"
+        ? displayMenuItems(menu)
+        : displayMenuItems(menuCategory);
+    });
+  });
+}
